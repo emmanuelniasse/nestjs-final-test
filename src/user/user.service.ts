cc623727cbb7ecd2abc32319a576ed603b677217
emmanuelniasse <emmanuelniasse@gmail.com>
2024-04-28 01:24:25 +0200

    feat: database service

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,29 +1,27 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { User } from '@prisma/client';
-import { PrismaService } from '../prisma.service';
+import { DatabaseService } from '../infrastructure/database/database.service';
+
 @Injectable()
 export class UserService {
-    constructor(private prisma: PrismaService) {}
+    constructor(private databaseService: DatabaseService) {}
 
     async addUser(email: string): Promise<User> {
         const userAlreadyExists = await this.getUser(email);
         if (userAlreadyExists) {
             throw new ConflictException('User with this email already exists');
         }
-        return this.prisma.user.create({
+        return this.databaseService.user.create({
             data: { email },
         });
     }
 
     getUser(email: string): Promise<unknown> {
-        return this.prisma.user.findUnique({ where: { email } });
+        return this.databaseService.user.findUnique({ where: { email } });
     }
 
-    async resetData(): Promise<void> {
-        try {
-            await this.prisma.user.deleteMany({});
-        } catch (error) {
-            throw error;
-        }
+    async resetData(): Promise<string> {
+        await this.databaseService.user.deleteMany();
+        return 'Users deleted';
     }
 }
